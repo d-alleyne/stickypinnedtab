37262016f082ae12a40b9330d300b9b4d9a31a99
Damien Alleyne <31164683+d-alleyne@users.noreply.github.com>
2021-06-03 18:08:23 -0400

    Initial implementation.

--- background_script.js
+++ background_script.js
@@ -1,1 +1,28 @@
-// Put all the javascript code here, that you want to execute in background.
+const pinnedTabs = new Map();
+const getPinnedTabs = () => browser.tabs.query({ pinned: true });
+getPinnedTabs().then(
+    tabs => {
+        tabs.forEach(tab => pinnedTabs.set(tab.id, { windowId: tab.windowId, pinned: true }));
+    }
+);
+
+const onPinned = (tabId, changeInfo, tab) => {
+    // the tab has been unpinned
+    if (pinnedTabs.has(tabId) && pinnedTabs.get(tabId).windowId === tab.windowId && !changeInfo.pinned) {
+        pinnedTabs.delete(tabId);
+    } else {
+        pinnedTabs.set(tabId, { windowId: tab.windowId, pinned: changeInfo.pinned });
+    }
+}
+
+browser.tabs.onUpdated.addListener(onPinned, { properties: ["pinned"] })
+
+const onAttached = (tabId) => {
+    // we have moved to a new window, so re-apply the pin
+    if (pinnedTabs.has(tabId)) {
+        browser.tabs.update(tabId, { pinned: true })
+            .then(tab => console.log(`Updated tab: ${tab.id}`), error => console.log(`Error: ${error}`));
+    }
+}
+
+browser.tabs.onAttached.addListener(onAttached);
