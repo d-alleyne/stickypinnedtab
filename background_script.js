3f666132ed06ee6990f0fa3d6018f6806f98a1ee
Damien Alleyne <31164683+d-alleyne@users.noreply.github.com>
2021-06-04 05:28:18 -0400

    Cleanup unused memory.

--- background_script.js
+++ background_script.js
@@ -1,34 +1,43 @@
 const pinnedTabs = new Map();
 const getPinnedTabs = () => browser.tabs.query({ pinned: true });
 
-getPinnedTabs().then(
-    tabs => tabs.forEach(
-        tab => pinnedTabs.set(tab.id, { windowId: tab.windowId, pinned: true })
-    )
+getPinnedTabs().then((tabs) =>
+  tabs.forEach((tab) =>
+    pinnedTabs.set(tab.id, { windowId: tab.windowId, pinned: true })
+  )
 );
 
 const onPinned = (tabId, changeInfo, tab) => {
-    // the tab has been unpinned
-    if (pinnedTabs.has(tabId) &&
-        pinnedTabs.get(tabId).windowId === tab.windowId &&
-        !changeInfo.pinned) {
-        pinnedTabs.delete(tabId);
-    } else {
-        pinnedTabs.set(tabId, { windowId: tab.windowId, pinned: changeInfo.pinned });
-    }
-}
+  // the tab has been unpinned
+  if (
+    pinnedTabs.has(tabId) &&
+    pinnedTabs.get(tabId).windowId === tab.windowId &&
+    !changeInfo.pinned
+  ) {
+    pinnedTabs.delete(tabId);
+  } else {
+    pinnedTabs.set(tabId, {
+      windowId: tab.windowId,
+      pinned: changeInfo.pinned,
+    });
+  }
+};
 
-browser.tabs.onUpdated.addListener(onPinned, { properties: ["pinned"] })
+browser.tabs.onUpdated.addListener(onPinned, { properties: ["pinned"] });
 
 const onAttached = (tabId) => {
-    // we have moved to a new window, so re-apply the pin
-    if (pinnedTabs.has(tabId)) {
-        browser.tabs.update(tabId, { pinned: true })
-            .then(
-                tab => console.log(`Updated tab: ${tab.id}`),
-                error => console.log(`Error: ${error}`)
-            );
-    }
-}
+  // we have moved to a new window, so re-apply the pin
+  if (pinnedTabs.has(tabId)) {
+    browser.tabs.update(tabId, { pinned: true }).then(
+      (tab) => console.log(`Updated tab: ${tab.id}`),
+      (error) => console.log(`Error: ${error}`)
+    );
+  }
+};
 
 browser.tabs.onAttached.addListener(onAttached);
+
+// memory cleanup
+browser.tabs.onRemoved.addListener(
+  (tabId) => pinnedTabs.has(tabId) && pinnedTabs.delete(tabId)
+);
